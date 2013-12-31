caece879b8ce4a467507e1ffeaf120d2d3943389
benji <git@benjiweber.co.uk>
2013-12-31 13:37:27 +0000

    Move tests to the top

--- src/test/java/uk/co/benjiweber/expressions/UsingTest.java
+++ src/test/java/uk/co/benjiweber/expressions/UsingTest.java
@@ -8,18 +8,6 @@ import static uk.co.benjiweber.expressions.Using.using;
 
 public class UsingTest {
 
-    static class Foo implements AutoCloseable {
-
-        public String result() {
-            return "result";
-        }
-
-        boolean closed = false;
-        public void close() throws Exception {
-            closed = true;
-        }
-    }
-
     @Test public void should_return_value() {
         String result = using(Foo::new, foo -> {
             return foo.result();
@@ -34,11 +22,6 @@ public class UsingTest {
         Assert.assertTrue(closeable.closed);
     }
 
-    Foo foo = new Foo();
-    Foo getClosable() {
-        return foo;
-    }
-
     @Test public void should_close_closeable_when_exception() {
         try {
             using(this::getClosable, foo -> {
@@ -50,6 +33,23 @@ public class UsingTest {
         }
 
         Assert.assertTrue(foo.closed);
+    }
+
+    Foo foo = new Foo();
+    Foo getClosable() {
+        return foo;
+    }
+
+    static class Foo implements AutoCloseable {
+
+        public String result() {
+            return "result";
+        }
+
+        boolean closed = false;
+        public void close() throws Exception {
+            closed = true;
+        }
     }
 
 }
