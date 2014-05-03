3ebf8e3118d44509eb56a27a82677d0a24e9c7fc
benji <git@benjiweber.co.uk>
2014-05-03 09:54:38 +0100

    Move test to top

--- src/test/java/uk/co/benjiweber/expressions/CaseTreeTest.java
+++ src/test/java/uk/co/benjiweber/expressions/CaseTreeTest.java
@@ -4,10 +4,30 @@ import org.junit.Test;
 import uk.co.benjiweber.expressions.caseclass.Case2;
 
 import static org.junit.Assert.assertEquals;
-import static uk.co.benjiweber.expressions.CaseTreeTest.Leaf.leaf;
-import static uk.co.benjiweber.expressions.CaseTreeTest.Node.node;
+import static uk.co.benjiweber.expressions.Leaf.leaf;
+import static uk.co.benjiweber.expressions.Node.node;
 
 public class CaseTreeTest {
+
+    @Test
+    public void counting_nodes_example() {
+        Tree tree =
+            node(5,
+                node(1, leaf, leaf),
+                node(3, leaf,
+                        node(4, leaf, leaf)
+                )
+            );
+
+        assertEquals(4, countNodes(tree));
+    }
+
+    int countNodes(Tree tree) {
+        return tree.match()
+                .when(Leaf.class, n -> 0)
+                .when(Node.class, n -> 1 + countNodes(n.left()) + countNodes(n.right()));
+    }
+}
 
     interface Tree extends Case2<Leaf, Node> {}
     interface Leaf extends Tree {
@@ -26,22 +46,3 @@ public class CaseTreeTest {
         }
     }
 
-    @Test
-    public void counting_nodes_example() {
-         Tree tree =
-             node(5,
-                node(1, leaf, leaf),
-                node(3, leaf,
-                    node(4, leaf, leaf)
-                )
-             );
-
-        assertEquals(4, countNodes(tree));
-    }
-
-    int countNodes(Tree tree) {
-        return tree.match()
-            .when(Leaf.class, n -> 0)
-            .when(Node.class, n -> 1 + countNodes(n.left()) + countNodes(n.right()));
-    }
-}
