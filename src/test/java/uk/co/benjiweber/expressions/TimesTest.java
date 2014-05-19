b6e8e4ec48892564627126a313caf969b5b0cc75
benji <git@benjiweber.co.uk>
2014-05-19 06:23:14 +0100

    Add a times(n).invoke(Action)

---
+++ src/test/java/uk/co/benjiweber/expressions/TimesTest.java
@@ -0,0 +1,41 @@
+package uk.co.benjiweber.expressions;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static uk.co.benjiweber.expressions.Times.times;
+
+public class TimesTest {
+
+    @Test
+    public void times_collection_size() {
+        List<String> aCollection = asList("one", "two", "three");
+
+        times(aCollection.size()).invoke(this::foo);
+
+        assertEquals(3, numberOfTimesFooWasInvoked);
+    }
+
+    @Test
+    public void times_literal() {
+        List<String> aCollection = asList("one", "two", "three");
+
+        times(6).invoke(this::bar);
+
+        assertEquals(6, numberOfTimesBarWasInvoked);
+    }
+
+
+    int numberOfTimesFooWasInvoked = 0;
+    private void foo() {
+        numberOfTimesFooWasInvoked++;
+    }
+
+    int numberOfTimesBarWasInvoked = 0;
+    private void bar() {
+        numberOfTimesBarWasInvoked++;
+    }
+}
